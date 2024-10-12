366c8524f432f0320f0ecdb4ddf8acdd03532568
symonmuchemi <simonkenya846@gmail.com>
2024-10-12 03:59:46 +0300

    update: imports and uses List from Typing package

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of all numbers in a list of floats.
 
     Args:
