d9c9f4338cf6aa6badfa5663ff2224a632b8ae25
symonmuchemi <simonkenya846@gmail.com>
2024-10-12 03:36:39 +0300

    task: returns floor of a floating point number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def floor(n: float) -> int:
+    """Returns the floor of a float number.
+
+    Args:
+        n (float): Floating point number.
+
+    Returns:
+        int: Floor.
+    """
+    return int(n)
