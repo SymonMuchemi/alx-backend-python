6ab677a01b17fddee60c0b1f784a959e8318697b
symonmuchemi <simonkenya846@gmail.com>
2024-10-12 03:43:12 +0300

    task: returns string rep of a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def to_str(n: float) -> str:
+    """Returns the string representation of a float.
+
+    Args:
+        n (float): A floating point number.
+
+    Returns:
+        str: String representation of n.
+    """
+    return str(n)
