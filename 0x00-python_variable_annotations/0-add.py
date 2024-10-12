84ea7a57d11cff058c6abb556fbdd0f746e29520
symonmuchemi <simonkenya846@gmail.com>
2024-10-12 03:23:09 +0300

    update: adds docstring

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
+    """sums up two floating point numbers.
+
+    Args:
+        a (float): first number.
+        b (float): second number.
+
+    Returns:
+        float: The summation result.
+    """
     return a + b
