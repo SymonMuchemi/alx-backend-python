a09e8043950bac0f695f931c0275da2f4700e0af
symonmuchemi <simonkenya846@gmail.com>
2024-10-12 03:53:28 +0300

    task: sums up a list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def sum_list(input_list: list[float]) -> float:
+    """Returns the sum of all numbers in a list of floats.
+
+    Args:
+        input_list (list[float]): List of floats.
+
+    Returns:
+        float: Sum.
+    """
+    return sum(input_list)
