de0600794be112268a5f52118835f9a30a01261d
symonmuchemi <simonkenya846@gmail.com>
2024-10-12 03:31:20 +0300

    task: annotates string concatenation function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def concat(str1: str, str2: str) -> str:
+    """Concats two strings.
+
+    Args:
+        str1 (str): First string.
+        str2 (str): Second string.
+
+    Returns:
+        str: Concatenated string result.
+    """
+    return str1 + str2
