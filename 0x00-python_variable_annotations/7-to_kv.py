dc4dd917f19d0cf00d38fc5bfbd8f06af77497f1
symonmuchemi <simonkenya846@gmail.com>
2024-10-15 05:56:47 +0300

    update: specifies return type in inline doc

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     """Takes a string and a float/int and return a tuple
-    of the string with the number squared.
+    of the string with the number squared as a float.
 
     Args:
         k (str): The string.
