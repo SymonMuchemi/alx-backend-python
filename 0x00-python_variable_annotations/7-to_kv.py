51cef84e4a09e5e42bb01444ca7008a8b352a02d
symonmuchemi <simonkenya846@gmail.com>
2024-11-26 09:31:34 +0300

    returns a tuple of str and float

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Takes a string and a float/int and return a tuple
     of the string with the number squared as a float.
 
