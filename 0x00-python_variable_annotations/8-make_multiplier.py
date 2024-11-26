28f52582708330a5158e8763bb870989a472c6b6
symonmuchemi <simonkenya846@gmail.com>
2024-11-26 09:51:15 +0300

    task: complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Make multiplier module"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[..., float]:
+    """Creates a multiplier function.
+
+    Args:
+        multiplier (float): Value to be squared.
+
+    Returns:
+        Callable[..., float]: a callable function that squares the multiplier.
+    """
+    def multiply(m: float) -> float:
+        """squares the m value.
+
+        Args:
+            m (float): The value to be multiplied.
+
+        Returns:
+            float: m * m.
+        """
+        return float(m**2)
+
+    return multiply
